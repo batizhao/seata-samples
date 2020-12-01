e65dec4fa83fa0772e46969a0117f5bb47348ad7
batizhao <zhaobati@gmail.com>
2020-12-01 14:26:46 +0800

    all in one on teledb.

--- springboot-mybatis/sql/all_in_one.sql
+++ springboot-mybatis/sql/all_in_one.sql
@@ -1,8 +1,3 @@
-# Account
-DROP SCHEMA IF EXISTS db_account;
-CREATE SCHEMA db_account;
-USE db_account;
-
 CREATE TABLE `account_tbl`
 (
     `id`      INT(11) NOT NULL AUTO_INCREMENT,
@@ -31,11 +26,6 @@ CREATE TABLE `undo_log` (
   UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-# Order
-DROP SCHEMA IF EXISTS db_order;
-CREATE SCHEMA db_order;
-USE db_order;
-
 CREATE TABLE `order_tbl`
 (
     `id`             INT(11) NOT NULL AUTO_INCREMENT,
@@ -47,25 +37,6 @@ CREATE TABLE `order_tbl`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-CREATE TABLE `undo_log` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `branch_id` bigint(20) NOT NULL,
-  `xid` varchar(100) NOT NULL,
-  `context` varchar(128) NOT NULL,
-  `rollback_info` longblob NOT NULL,
-  `log_status` int(11) NOT NULL,
-  `log_created` datetime NOT NULL,
-  `log_modified` datetime NOT NULL,
-  `ext` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-# Storage
-DROP SCHEMA IF EXISTS db_storage;
-CREATE SCHEMA db_storage;
-USE db_storage;
-
 CREATE TABLE `storage_tbl`
 (
     `id`             INT(11) NOT NULL AUTO_INCREMENT,
@@ -76,19 +47,5 @@ CREATE TABLE `storage_tbl`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-
 INSERT INTO storage_tbl (id, commodity_code, count)
 VALUES (1, '2001', 1000);
-
-CREATE TABLE `undo_log` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `branch_id` bigint(20) NOT NULL,
-  `xid` varchar(100) NOT NULL,
-  `context` varchar(128) NOT NULL,
-  `rollback_info` longblob NOT NULL,
-  `log_status` int(11) NOT NULL,
-  `log_created` datetime NOT NULL,
-  `log_modified` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
